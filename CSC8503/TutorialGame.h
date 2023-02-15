b1d1cd9e381dd1104364a9ccb4a965420034a877
scottNicho <120742244+scottNicho@users.noreply.github.com>
2023-02-15 15:18:27 +0000

    Update TutorialGame.h
    additions:
    Projectile* AddBulletToWorld function DEC -> superfluous now
    Projectile* useRecycledBulet function DEC -> superfluous now
    Projectile* useNewBullet function DEC -> gets a object from the object pool vector then initialises it.
    Projectile* FireBullet function DEC ->  encapsulates use new bullet and applies force when called.
    The add player To world function now returns the player as a player Tracking pointer (inherited from the origonal game object)
    
    addToRunningBulletTime Function increments the variable running bullet time
    
    resetRunning bulletTime funciton resets this time
    
    runningbullettimeTest function compares the size of the running time against an input then rests the time
    
    addToBulletDeleteTime, resetBulletDeleteTime and BulletDeleteTimeTest are all not currently used
    
    Member variables
    The members bulletLifeLimit and bulletDeletionTime are not currently used but can be used as const limits for deleting bullets
    running bullet time is used to increment a timer every time the games update function is called so that events such as bullet firing rate can be regulated.
    bullet deletion time is not currently used.
    objectpool is a pointer to the Object Pool class used to house and manage the objects used as bullets.

--- CSC8503/TutorialGame.h
+++ CSC8503/TutorialGame.h
@@ -4,19 +4,64 @@
 #include "GameTechVulkanRenderer.h"
 #endif
 #include "PhysicsSystem.h"
-
+#include"playerTracking.h"
 #include "StateGameObject.h"
+#include"Projectile.h"
+//#include "C:\Users\c2065963\source\repos\Advanced compter physics\Build\CSC8503\TimeKeeper.h"
 
-#include <array>
+#include "ObjectPool.h"
 
 namespace NCL {
 	namespace CSC8503 {
+		class AgentMovement;
 		class TutorialGame		{
 		public:
 			TutorialGame();
 			~TutorialGame();
 
-			virtual void UpdateGame(float dt);
+			virtual int UpdateGame(float dt); //testing changing update game to int return "had to try it :)"
+			//Timekeeper         runningtime;
+			void UpdateGameRenderer(float dt);
+
+
+			void InitWorld(); //moved from protected
+			void InitWorldtest();
+			void InitWorldtest2();
+
+			/*void updateBehaviourTree(BehaviourSequence* root1);*/
+			void setLockedObjectNull();
+
+			void addToRunningBulletTime(float timeIncrement) {
+				runnigBulletTime += timeIncrement;
+			}
+
+			void resetRunningBulletTime() {
+				runnigBulletTime = 0.0f;
+			}
+
+			bool runningBulletTimeLimitTest(float rateFireLim) {
+				if (runnigBulletTime >= rateFireLim) {
+					resetRunningBulletTime();
+					return true;
+				}
+				return false;
+			}
+
+			void addToBulletDeleteTime(float timeIncrement) {
+				BulletDeleteTime += timeIncrement;
+			}
+			
+			void resetBulletDeleteTime() {
+				BulletDeleteTime = 0.0f;
+			}
+
+			bool bulletDeleteTimeTest() {
+				if (BulletDeleteTime >= bulletDeletionLimit) {
+					resetBulletDeleteTime();
+					return true;
+				}
+				return false;
+			}
 
 		protected:
 			void InitialiseAssets();
@@ -24,7 +69,11 @@ namespace NCL {
 			void InitCamera();
 			void UpdateKeys();
 
-			void InitWorld();
+			void BridgeConstraintTest();
+			void chainBallTest();
+
+			void setGoatCharacter(playerTracking* assignCharcter);
+			void setEnemyGoat(GameObject* assignCharcter);
 
 			/*
 			These are some of the world/object creation functions I created when testing the functionality
@@ -35,22 +84,40 @@ namespace NCL {
 
 			void InitSphereGridWorld(int numRows, int numCols, float rowSpacing, float colSpacing, float radius);
 			void InitMixedGridWorld(int numRows, int numCols, float rowSpacing, float colSpacing);
+			void InitMixedGridWorldtest(int numRows, int numCols, float rowSpacing, float colSpacing);
+			void InitMixedGridWorldGoatStrip(int numRows, int numCols, float rowSpacing, float colSpacing);
 			void InitCubeGridWorld(int numRows, int numCols, float rowSpacing, float colSpacing, const Vector3& cubeDims);
 
 			void InitDefaultFloor();
+			void InitDefaultFloorRunway();
 
 			bool SelectObject();
 			void MoveSelectedObject();
 			void DebugObjectMovement();
 			void LockedObjectMovement();
+			void movePlayer(playerTracking* unitGoat);
+			void ResetPhantomCube(GameObject* cubeToReset);
+			void setLockedObject(GameObject* goatPlayer);
 
 			GameObject* AddFloorToWorld(const Vector3& position);
+			GameObject* AddRunwayToWorld(const Vector3& position);
 			GameObject* AddSphereToWorld(const Vector3& position, float radius, float inverseMass = 10.0f);
+			Projectile* AddBulletToWorld(playerTracking* playableCharacter);
+			Projectile* useNewBullet(playerTracking* passedPlayableCharacter);
+			
+			Projectile* useRecycledBulet(playerTracking* passedPlayableCharacter);
+			Projectile* FireBullet(playerTracking* selectedPlayerCharacter);
 			GameObject* AddCubeToWorld(const Vector3& position, Vector3 dimensions, float inverseMass = 10.0f);
+			GameObject* AddDestructableCubeToWorld(const Vector3& position, Vector3 dimensions, float dt,float inverseMass = 10.0f);
+			GameObject* AddPhantomCubeToWorld(const Vector3& position, Vector3 dimensions, float inverseMass = 10.0f);
+			GameObject* AddPhantomBoxToWorld(const Vector3& position, Vector3 dimensions, float inverseMass);
 
-			GameObject* AddPlayerToWorld(const Vector3& position);
+			playerTracking* AddPlayerToWorld(const Vector3& position);
+			GameObject* AddEnemyGoatToWorld(const Vector3& position);
 			GameObject* AddEnemyToWorld(const Vector3& position);
 			GameObject* AddBonusToWorld(const Vector3& position);
+
+
 
 #ifdef USEVULKAN
 			GameTechVulkanRenderer*	renderer;
@@ -59,13 +126,17 @@ namespace NCL {
 #endif
 			PhysicsSystem*		physics;
 			GameWorld*			world;
+			
 
 			bool useGravity;
 			bool inSelectionMode;
 
 			float		forceMagnitude;
 
+			//BehaviourSequence* newrootSequence = nullptr;
+
 			GameObject* selectionObject = nullptr;
+			GameObject* phantomCubeOutput = nullptr;
 
 			MeshGeometry*	capsuleMesh = nullptr;
 			MeshGeometry*	cubeMesh	= nullptr;
@@ -81,25 +152,34 @@ namespace NCL {
 
 			//Coursework Additional functionality	
 			GameObject* lockedObject	= nullptr;
-			Vector3 lockedOffset		= Vector3(0, 14, 20);
+			Vector3 lockedOffset		= Vector3(0, 7, 20);
 			void LockCameraToObject(GameObject* o) {
 				lockedObject = o;
 			}
 
-			GameObject* objClosest = nullptr;
 
-			GameObject* worldFloor;
-			
-			//this was me
-			OGLComputeShader* computeShader;
-			void RunComputeShader(GameObject* floor,int width, int height, int leftS, int rightS, int topT, int bottomT, int radius,Vector2 center);
+			StateGameObject* AddStateObjectToWorld(const Vector3& position);
+			AgentMovement* amovement = nullptr;
+			StateGameObject* testStateObject;
+			GameObject* objClosest = nullptr;
+			playerTracking* goatCharacter = nullptr;
+			GameObject* EnemyGoat = nullptr;
+			playerTracking* testplayer = nullptr;
+
+			const int bulletLifeLimit = 2;
+			const int bulletDeletionLimit = 5;
+			float runnigBulletTime = 0.0f;
+			float BulletDeleteTime = 0.0f;
+
+			ObjectPool<Projectile> * objectpool;
+
+			/*OGLComputeShader* computeShader;
+			void RunComputeShader(GameObject* floor, int width, int height, int leftS, int rightS, int topT, int bottomT, int radius, Vector2 center);
 			OGLShader* quadShader;
 			OGLTexture* quadTex = nullptr;
 			void InitQuadTexture();
 			TextureBase* floorTex = nullptr;
-			void InitPaintableTextureOnObject(GameObject* object);
-
+			void InitPaintableTextureOnObject(GameObject* object);*/
 		};
 	}
 }
-

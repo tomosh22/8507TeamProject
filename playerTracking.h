bda6601302af06701e4ae757c8f8f2907e337481
scottNicho <120742244+scottNicho@users.noreply.github.com>
2023-02-15 13:46:54 +0000

    Add files via upload
    Player tracking class. interacts with the projectile class and keeps track of stats relating to game play such as firing projectiles and player Id. 
    Inherited from Game object class.

---
+++ playerTracking.h
@@ -0,0 +1,92 @@
+#pragma once
+
+#include"GameObject.h"
+#include"Projectile.h"
+#include<vector>
+#include"Vector4.h"
+
+
+namespace NCL::CSC8503 {
+
+	class playerTracking :public GameObject {
+	public:
+
+		playerTracking();
+
+		~playerTracking() {
+			delete playerProjectile;
+			bulletsUsed.clear();
+			bulletsUsedAndMoved.clear();
+
+		}
+
+		void setplayerID(int assignedPlayerID) {
+			playerID = assignedPlayerID;
+		}
+
+		void setTeamID(int assignedTeamID) {
+			playerID = assignedTeamID;
+		}
+
+		void resetPlayerProjectile() {
+			playerProjectile = nullptr;
+		}
+
+		void setWeponType(gun newWeponType) {
+			type = newWeponType;
+		}
+
+		void setPaintColor(Vector4 newPaintColor) {
+			paintColor = newPaintColor;
+		}
+
+		void addToBulletsUsed(Projectile* bulletToAdd) {
+			bulletsUsed.push_back(bulletToAdd);
+		}
+
+		void clearBulletsUsed();
+
+		void updateBulletsUsed();
+
+		 int getBulletVectorSize() {
+			return bulletsUsed.size();
+		}
+
+		 Projectile* reuseBullet();
+
+		Vector4 getPaintColor() {
+			return paintColor;
+		}
+
+		gun getWeponType() {
+			return type;
+		}
+
+		/*void AssignPlayerWeapon(gun weponType) {
+			playerProjectile->setGunType(weponType);
+		}*/
+
+		Projectile* getPlayerProjectile() {
+			return playerProjectile;
+		}
+        
+
+	protected:
+		 
+		 float playerYawOrientation;
+		 float playerPitchOrientation;
+		 int playerID;
+		 int teamID;
+		 int IndividualplayerScore;
+		 Projectile* playerProjectile;
+		 gun type;
+		 Vector4 paintColor;
+		 vector<Projectile*> bulletsUsed;
+		 vector<Projectile*> bulletsUsedAndMoved;
+	};
+
+
+
+
+
+}

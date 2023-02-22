b7173b194cd689fc1ae67de23264bff4f5602409
scottNicho <120742244+scottNicho@users.noreply.github.com>
2023-02-22 12:51:07 +0000

    Update PhysicsSystem.h
    Inclusion of collision data struct to house data when a bullet collides.
    Basic collision detection changed to collisionData type.

--- CSC8503CoreClasses/PhysicsSystem.h
+++ CSC8503CoreClasses/PhysicsSystem.h
@@ -7,6 +7,13 @@ namespace NCL {
 		public:
 			PhysicsSystem(GameWorld& g);
 			~PhysicsSystem();
+			
+			struct collisionData
+			{
+				float paintRadius;
+				Vector3 contactPosition;
+
+			};
 
 			void Clear();
 
@@ -26,7 +33,7 @@ namespace NCL {
 		protected:
 
 
-			int BasicCollisionDetection();
+			collisionData BasicCollisionDetection();
 			void BroadPhase();
 			void NarrowPhase();
 

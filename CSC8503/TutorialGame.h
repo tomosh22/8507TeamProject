613c733de4768b0c1e887c58233804df792bd198
scottNicho <120742244+scottNicho@users.noreply.github.com>
2023-03-21 21:51:40 +0000

    Update TutorialGame.h
    toggle function for the pause bool has been added
    a float to keep track of the game time and a function to add time to it and one to retrieve that time.

--- CSC8503/TutorialGame.h
+++ CSC8503/TutorialGame.h
@@ -1,30 +1,62 @@
 #pragma once
+
 #include "GameTechRenderer.h"
 #ifdef USEVULKAN
 #include "GameTechVulkanRenderer.h"
 #endif
 #include "PhysicsSystem.h"
+
 #include"playerTracking.h"
+
+
 #include "StateGameObject.h"
-#include"Projectile.h"
 #include <array>
 #include"ObjectPool.h"
+#include "RenderObject.h"
+#include "PropSystem.h"
+
+#include"MeshAnimation.h"
+#include"MeshMaterial.h"
+
+#include <thread>
+#include <mutex>
+
+#include "RayMarchSphere.h"
 
 namespace NCL {
 	namespace CSC8503 {
-		const int GAME_MODE_DEFAULT = 0;
-		const int GAME_MODE_GRAPHIC_TEST = 1;
-		const int GAME_MODE_PHISICAL_TEST = 2;
-		class TutorialGame		{
+		const enum GameMode {
+			GAME_MODE_DEFAULT,
+			GAME_MODE_GRAPHIC_TEST,
+			GAME_MODE_SINGLE_GAME,
+			GAME_MODE_ONLINE_GAME,
+			GAME_MODE_SELECT_TEAM,
+		};
+
+		const int TWO_PLAYERS = 2;
+		const int FOUR_PLAYERS = 4;
+
+		enum TeamID {
+			TEAM_DEFAULT,
+			TEAM_RED,
+			TEAM_BLUE,
+			TEAM_GREEN,
+			TEAM_YELLOW,
+		};
+
+		class Projectile;
+		class TutorialGame {
 		public:
 			TutorialGame();
 			~TutorialGame();
 
 			virtual void UpdateGame(float dt);
-			void SelectMode();
+			void SelectGameWorld();
+			int SelectTeam();
 			//void InitWorld(); //moved from protected
 			void InitGraphicTest();
-			void InitPhysicalTest();
+			void InitSingleGameMode();
+			void InitOnlineGame(int treamID);
 			void InitWorldtest2();
 
 			void setLockedObjectNull();
@@ -45,7 +77,45 @@ namespace NCL {
 				return false;
 			}
 
+			float GetGameTime() {
+				return gameTime;
+			}
+
+			void AddToGameTime(float timeIncrement) {
+				gameTime += timeIncrement;
+			}
+
 			void setEnemyGoat(GameObject* assignCharcter);
+
+
+			void TogglePause() {
+				pause = !pause;
+			}
+
+			void DispatchComputeShaderForEachTriangle(GameObject* object, Vector3 spherePosition, float sphereRadius, int teamID, bool clearMask = false);
+
+			MeshGeometry* capsuleMesh = nullptr;
+			MeshGeometry* cubeMesh = nullptr;
+			MeshGeometry* sphereMesh = nullptr;
+
+			//this was me
+			MeshGeometry* triangleMesh = nullptr;
+			MeshGeometry* monkeyMesh = nullptr;
+			MeshGeometry* floorMesh = nullptr;
+			MeshGeometry* maxMesh = nullptr;
+			MeshGeometry* basicWallMesh = nullptr;
+			MeshGeometry* bunnyMesh = nullptr;
+
+			TextureBase* basicTex = nullptr;
+			ShaderBase* basicShader = nullptr;
+
+
+			//Coursework Meshes
+			MeshGeometry* charMesh = nullptr;
+			MeshGeometry* enemyMesh = nullptr;
+			MeshGeometry* bonusMesh = nullptr;
+
+			MeshGeometry* playerMesh = nullptr;
 
 		protected:
 			void InitialiseAssets();
@@ -62,7 +132,7 @@ namespace NCL {
 			in the module. Feel free to mess around with them to see different objects being created in different
 			test scenarios (constraints, collision types, and so on). 
 			*/
-			void InitGameExamples();
+			void InitGameObjects();
 
 			void InitSphereGridWorld(int numRows, int numCols, float rowSpacing, float colSpacing, float radius);
 			void InitMixedGridWorld(int numRows, int numCols, float rowSpacing, float colSpacing);
@@ -74,16 +144,26 @@ namespace NCL {
 			void InitDefaultFloor();
 			void InitDefaultFloorRunway();
 
+			void UpdateWorldCamera(float dt);
+			void CameraLockOnPlayer();
+			void RayCast();
+			void SelectMode();
 			bool SelectObject();
-			void MoveSelectedObject();
-			void DebugObjectMovement();
 			void LockedObjectMovement();
+			void ControlPlayer(float dt);
 			void movePlayer(playerTracking* unitGoat);
 			void setLockedObject(GameObject* goatPlayer);
 
+			class RayMarchSphere : public GameObject {
+			public:
+				Vector3 center;
+				float radius;
+				Vector3 color;
+			};
 
 			GameObject* AddFloorToWorld(const Vector3& position, const Vector3& scale, bool rotated = false);
-			GameObject* AddSphereToWorld(const Vector3& position, float radius, bool render, float inverseMass = 10.0f);
+			GameObject* AddSphereToWorld(const Vector3& position, float radius, bool render, float inverseMass = 10.0f, bool physics = true);
+			GameObject* AddRayMarchSphereToWorld(const Vector3& position, float radius);
 			
 
 			
@@ -98,8 +178,9 @@ namespace NCL {
 			GameObject* AddCapsuleToWorld(const Vector3& position, float halfHeight, float radius, float inversMass = 1.0f);
 			
 
-			GameObject* AddMonkeyToWorld(const Vector3& position, Vector3 dimensions, float inverseMass = 10.0f);
+			GameObject* AddMonkeyToWorld(const Vector3& position, Vector3 dimensions, float inverseMass = 10.0f, bool physics = true);
 			GameObject* AddMaxToWorld(const Vector3& position, Vector3 dimensions, float inverseMass = 10.0f);
+			GameObject* AddBunnyToWorld(const Vector3& position, Vector3 dimensions, float inverseMass = 10.0f, bool physics = true);
 			GameObject* AddWallToWorld(const Vector3& position, Vector3 dimensions, float inverseMass = 10.0f);
 
 			playerTracking* AddPlayerToWorld(const Vector3& position, Quaternion& orientation);
@@ -110,45 +191,34 @@ namespace NCL {
 			//this was me
 			GameObject* AddDebugTriangleToWorld(const Vector3& position);
 
+			void AddMapToWorld();
+
 #ifdef USEVULKAN
 			GameTechVulkanRenderer*	renderer;
 #else
+
 			GameTechRenderer* renderer;
 #endif
+
 			PhysicsSystem*		physics;
-			GameWorld*			world;
 
 			bool useGravity;
 			bool inSelectionMode;
 
 			float		forceMagnitude;
 
+			Vector3 viewOffset = Vector3(10.0f, 3.0f, 10.0f);
+
 			GameObject* selectionObject = nullptr;
 			GameObject* phantomCubeOutput = nullptr;
 
-			MeshGeometry*	capsuleMesh = nullptr;
-			MeshGeometry*	cubeMesh	= nullptr;
-			MeshGeometry*	sphereMesh	= nullptr;
 
-			//this was me
-			MeshGeometry* triangleMesh = nullptr;
-			MeshGeometry* monkeyMesh = nullptr;
-			MeshGeometry* floorMesh = nullptr;
-			MeshGeometry* maxMesh = nullptr;
-			MeshGeometry* basicWallMesh = nullptr;
-
-			TextureBase*	basicTex	= nullptr;
-			ShaderBase*		basicShader = nullptr;
-
-			//Coursework Meshes
-			MeshGeometry*	charMesh	= nullptr;
-			MeshGeometry*	enemyMesh	= nullptr;
-			MeshGeometry*	bonusMesh	= nullptr;
 
 			//Coursework Additional functionality	
 			GameObject* lockedObject	= nullptr;
 			//Vector3 lockedOffset		= Vector3(0, 14, 20); - origonal
 			Vector3 lockedOffset = Vector3(0, 7, 20);
+
 			void LockCameraToObject(GameObject* o) {
 				lockedObject = o;
 			}
@@ -158,9 +228,9 @@ namespace NCL {
 
 			GameObject* objClosest = nullptr;
 
-			playerTracking* goatCharacter = nullptr;
+			//playerTracking* goatCharacter = nullptr;
 			GameObject* EnemyGoat = nullptr;
-			playerTracking* testplayer = nullptr;
+			playerTracking* playerObject = nullptr;
 
 			const int bulletLifeLimit = 2;
 			const int bulletDeletionLimit = 5;
@@ -169,23 +239,29 @@ namespace NCL {
 
 			ObjectPool<Projectile>* objectpool;
 
-			GameObject* worldFloor;
+			
 			
 			//this was me
 			OGLComputeShader* computeShader;
 			void RunComputeShader(GameObject* floor,int width, int height, int leftS, int rightS, int topT, int bottomT, int radius,Vector2 center, int teamID);
 			OGLShader* quadShader;
-			TextureBase* quadTex = nullptr;
+			
 			void InitQuadTexture();
 			TextureBase* floorTex = nullptr;
 			void InitPaintableTextureOnObject(GameObject* object, bool rotated = false);
 
-			void DispatchComputeShaderForEachTriangle(GameObject* object, Vector3 spherePosition, float sphereRadius);
+
+			
+
 			GLuint triangleSSBO;
+			GLuint debugTriangleSSBO;
 			void SetUpTriangleSSBOAndDataTexture();
 			OGLComputeShader* triComputeShader;
+			OGLComputeShader* triRasteriseShader;
 			//OGLTexture* triDataTex;//1d texture
-			//GLuint triangleBoolSSBO;
+			GLuint triangleBoolSSBO;
+			GLuint triangleRasteriseSSBO;
+			GLuint triangleRasteriseSSBOSecondShader;
 			bool SphereTriangleIntersection(Vector3 sphereCenter, float sphereRadius, Vector3 v0, Vector3 v1, Vector3 v2, Vector3& intersectionPoint);
 
 			void DispatchComputeShaderForEachPixel();
@@ -194,12 +270,9 @@ namespace NCL {
 			float hitDistance;
 			float noHitDistance;
 			float debugValue;
-			class RayMarchSphere : public GameObject {
-			public:
-				Vector3 center;
-				float radius;
-				Vector3 color;
-			};
+
+			
+
 			std::vector<GameObject*> spheres;
 			std::vector<RayMarchSphere*> rayMarchSpheres;
 			GLuint rayMarchSphereSSBO;
@@ -215,7 +288,9 @@ namespace NCL {
 			std::array<char, 1000 * 1000> zeros;
 			GameObject* testTriangle;
 			GameObject* monkey;
+			GameObject* bunny;
 			void AddDebugTriangleInfoToObject(GameObject* object);
+			
 			TextureBase* metalTex;
 			TextureBase* testBumpTex;
 			void SendRayMarchData();
@@ -225,7 +300,78 @@ namespace NCL {
 			void UpdateRayMarchSpheres();
 
 			int gameMode = GAME_MODE_DEFAULT;
+
+			GLuint tempSSBO;
+
+			
+			//Team currentTeam = Team::team2;
+			//int currentTeamInt = 1;
+			
+			int highestTriCount = 0;
+
+			TextureBase* ironDiffuse = nullptr;
+			TextureBase* ironBump = nullptr;
+			TextureBase* ironMetallic = nullptr;
+			TextureBase* ironRoughness = nullptr;
+
+			TextureBase* crystalDiffuse = nullptr;
+			TextureBase* crystalBump = nullptr;
+			TextureBase* crystalMetallic = nullptr;
+			TextureBase* crystalRoughness = nullptr;
+			TextureBase* crystalHeightMap = nullptr;
+			TextureBase* crystalEmissionMap = nullptr;
+			TextureBase* crystalAOMap = nullptr;
+			TextureBase* crystalOpacityMap = nullptr;
+			TextureBase* crystalGlossMap = nullptr;
+
+			TextureBase* spaceShipDiffuse = nullptr;
+			TextureBase* spaceShipBump = nullptr;
+			TextureBase* spaceShipMetallic = nullptr;
+			TextureBase* spaceShipRoughness = nullptr;
+			TextureBase* spaceShipHeightMap = nullptr;
+			TextureBase* spaceShipEmissionMap = nullptr;
+			TextureBase* spaceShipAOMap = nullptr;
+			TextureBase* spaceShipOpacityMap = nullptr;
+			TextureBase* spaceShipGlossMap = nullptr;
+
+			PBRTextures* crystalPBR;
+			PBRTextures* spaceShipPBR;
+			PBRTextures* rockPBR;
+			PBRTextures* grassWithWaterPBR;
+			PBRTextures* fencePBR;
+			
+
+			GameObject* testSphere0 = nullptr;
+			GameObject* testSphere1 = nullptr;
+			GameObject* testSphere2 = nullptr;
+			GameObject* testSphere3 = nullptr;
+			GameObject* testSphere4 = nullptr;
+
+			bool rayMarch = true;
+			//Player Animation 
+			OGLShader* characterShader;
+			MeshMaterial* playerMaterial;
+			int currentFrame;
+			float frameTime;
+			float gameTime = 0.0f;
+			void UpdateAnimations(float dt);
+			std::vector<RenderObject*> animatedObjects;
+
+			bool pause = false;
+
+			int playerNum = 0;
+			
+
 		};
+
+		/*
+
+		Each of the little demo scenarios used in the game uses the same 2 meshes,
+		and the same texture and shader. There's no need to ever load in anything else
+		for this module, even in the coursework, but you can add it if you like!
+
+		*/
+
 	}
 }
 
